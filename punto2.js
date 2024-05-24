a19e2982076e700340aac59fcf300aec98af5e38
devbastian08 <sebastian08.dev@gmail.com>
2024-05-23 21:24:29 -0500

    p2 terminado

--- punto2.js
+++ punto2.js
@@ -1,26 +1,34 @@
-let ages = [17, 18, 20, 15, 16, 19];
+let ages = [];
 let menores = 0;
 let adultos = 0;
-
-for (let i = 0; i < ages.length; i++) {
-  if (ages[i] >= 18) {
+let admayor = 0;
+// evaluacion del rango de edad 1 entre 120
+for (let i = 0; i < 10; i++) {
+  ages[i] = parseInt(prompt("escriba la edad de una persona "));
+  while (ages[i] < 1 || ages[i] > 120) {
+    ages[i] = parseInt(prompt("escriba la edad correcta de la persona "));
+  }
+}
+for (let j = 0; j < 10; j++) {
+  //evaliuacion de personas adultas mayores
+  if (ages[j] >= 60) {
+    admayor++;
+  }
+  //evaluacion de personas menores o mayores de edad
+  if (ages[j] >= 18) {
     adultos++;
   } else {
     menores++;
   }
 }
-
-console.log("Menores:", menores);
-console.log("Adultos:", adultos);
-
-var min = Math.min(...ages);
-console.log("Min:", min)
-
-var max = Math.max(...ages);
-console.log("Max:", max)
- 
-var sum = ages.reduce((a, b) => a + b, 0);
-console.log("Sum:", sum)
-
-var promedio = sum / ages.length;
-console.log("Promedio:", promedio)
+console.log(ages);
+console.log("la cantidad de personas adultas mayores son: ", admayor);
+console.log("Las personas menores de edad son: ", menores);
+console.log("Las personas mayores de edad son: ", adultos);
+let min = Math.min(...ages);
+console.log("La edad mas baja es:", min);
+let max = Math.max(...ages);
+console.log("La edad mas alta es:", max);
+let sum = ages.reduce((a, b) => a + b, 0);
+let promedio = sum / ages.length;
+console.log("El Promedio de edad es: ", promedio);
